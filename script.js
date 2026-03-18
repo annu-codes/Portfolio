5251b3e883e343b842d4a47382dd1edb734bed77
Anushka Maurya <anushkamaurya211@gmail.com>
2026-03-18 16:22:03 +0530

    Add files via upload
    Uploaded HTML ,CSS, JavaScript files, resume and profile image for portfolio hosting

---
+++ script.js
@@ -0,0 +1,22 @@
+
+
+
+const text = "Frontend Developer | C++ Programmer";
+
+let i = 0;
+
+function typing(){
+
+if(i < text.length){
+
+document.querySelector(".typing").innerHTML += text.charAt(i);
+
+i++;
+
+setTimeout(typing,100);
+
+}
+
+}
+
+typing();
